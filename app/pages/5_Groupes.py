44658f05c047d1cebabe01e374d33ad6d276fce4
Mattan-Sania <saniaamattan@gmail.com>
2026-03-30 17:25:45 +0200

    Enhance film grouping page: update description and implement clustering functionality with dynamic group selection

--- app/pages/5_Groupes.py
+++ app/pages/5_Groupes.py
@@ -4,6 +4,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 import streamlit as st
 from utils.loadmovies import loadmovies
+from models.kmoy import k_moyennes
 
 st.title("Groupes de films")
-st.write("Cette page utilisera l'algorithme des k-moyennes.")
+st.write("Cette page utilisera l'algorithme des k-moyennes afin de regrouper automatiquement les films en plusieurs catégories.")
+
+movies = loadmovies()
+k = st.slider("Choisis le nombre de groupes", 2, 6, 3)
+
+if st.button("Lancer le clustering"):
+    c, clus = k_moyennes(movies, k)
+    
+    st.success(f"{k} groupes ont été crées")
+    
+    for i in range(len(clus)):
+        with st.expander(f"Groupe {i+1} ({len(clus[i])})"):
+            if clus[i]==[]:
+                st.write("Aucun film dans ce groupe.")
+            else:
+                for film in clus[i]:
+                    st.write("- " + film["title"])
+      
